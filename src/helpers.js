7f543ef5eeb61942f8657aa0572037ae138d44b4
javierbrea <javier.brea@gmail.com>
2022-05-27 09:45:42 +0200

    chore: archive repository

--- src/helpers.js
+++
@@ -1,5 +0,0 @@
-export const deprecatedMethod = (method, newMethod) => {
-  console.warn(
-    `@data-provider/react: "${method}" is deprecated. Please use "${newMethod}" instead.`
-  );
-};
--- test/index.spec.js
+++
@@ -1,7 +0,0 @@
-const index = require("../src/index");
-
-describe("index", () => {
-  it("should export useRefresh", () => {
-    expect(index.useRefresh).toBeDefined();
-  });
-});
